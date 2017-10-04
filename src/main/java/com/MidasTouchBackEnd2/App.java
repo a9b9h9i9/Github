ea6161474bf8b2e33149a4c181ae99eb81a9e0de
a9b9h9i9 <32479307+a9b9h9i9@users.noreply.github.com>
2017-10-04 09:43:28 +0530

    my upload'

---
+++ src/main/java/com/MidasTouchBackEnd2/App.java
@@ -0,0 +1,13 @@
+package com.MidasTouchBackEnd2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
